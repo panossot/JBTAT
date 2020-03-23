e5cae4be1eac21ea5069d92e09c8e79555d69b32
Panagiotis Sotiropoulos <psotirop@redhat.com>
2020-03-23 10:15:53 +0100

    Adding EnhancedThreadQueueExecutorTestCase ...

---
+++ modules/src/main/java/org/jboss/threads/additional/testsuite/jdkall/present/threadTestsuite/EnhancedThreadQueueExecutorTestCase.java
@@ -0,0 +1,60 @@
+package org.jboss.threads;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.jboss.eap.additional.testsuite.annotations.EapAdditionalTestsuite;
+
+@EapAdditionalTestsuite({"modules/testcases/jdkAll/master/threadTestsuite/src/main/java"})
+public class EnhancedThreadQueueExecutorTestCase {
+
+    @Test
+    public void testEnhancedExecutorShutdownNoTasks() throws Exception {
+        final CountDownLatch terminateLatch = new CountDownLatch(1);
+        EnhancedQueueExecutor executor = new EnhancedQueueExecutor.Builder()
+                .setCorePoolSize(10)
+                .setKeepAliveTime(1, TimeUnit.NANOSECONDS)
+                .setTerminationTask(new Runnable() {
+                    @Override
+                    public void run() {
+                        terminateLatch.countDown();
+                    }
+                })
+                .build();
+
+        executor.shutdown();
+        Assert.assertTrue(terminateLatch.await(10, TimeUnit.SECONDS));
+    }
+
+    @Test //JBTHR-50
+    public void testEnhancedExecutorShutdown() throws Exception {
+        final CountDownLatch terminateLatch = new CountDownLatch(1);
+        EnhancedQueueExecutor executor = new EnhancedQueueExecutor.Builder()
+                .setCorePoolSize(10)
+                .setKeepAliveTime(1, TimeUnit.NANOSECONDS)
+                .setTerminationTask(new Runnable() {
+                    @Override
+                    public void run() {
+                        terminateLatch.countDown();
+                    }
+                })
+                .build();
+
+        for(int i = 0; i < 10000; ++i) {
+            executor.submit(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        //ignore
+                    }
+                }
+            });
+        }
+        executor.shutdown();
+        Assert.assertTrue(terminateLatch.await(10, TimeUnit.SECONDS));
+    }
+}
